101ec24a5d02efc1b2aa2269dd2c53581c8c6fb5
logeshkumar pushpadass <logeshforaws34@outlook.com>
2026-09-06 06:40:56 +0000

    yes

--- models/staging/department.sql
+++ models/staging/department.sql
@@ -1,3 +1,3 @@
 {{config(materialized='view')}}
 
-select * from {{ source('raw', 'department') }}
+select department_id,department_name, department_id||'-'||department_name as department_id_name from {{ source('raw', 'department') }}
