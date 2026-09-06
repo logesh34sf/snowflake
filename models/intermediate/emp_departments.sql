ee6b86cc6c9082135cea0d419e363497a7ad4ab7
logeshkumar pushpadass <logeshforaws34@outlook.com>
2026-09-06 05:38:46 +0000

    yes

---
+++ models/intermediate/emp_departments.sql
@@ -0,0 +1,6 @@
+{{config(materialized='table')}}
+with emp as(
+select * from {{ ref('employee') }}) ,
+dep as (select * from {{ ref('department') }})
+select * from emp e join dep d on d.department_name=e.department
+
--- models/staging/employee.sql
+++ models/staging/employee.sql
@@ -11,7 +11,7 @@
 
 with source_data as (
 
-   select * from {{ source('raw', 'customers') }}
+   select * from {{ source('raw', 'employee') }}
 
 )
 
