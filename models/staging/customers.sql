860ae9a39dde1ea323bc0a2f7dab1a8f4d577d9e
logeshkumar pushpadass <logeshforaws34@outlook.com>
2026-09-06 05:25:01 +0000

    sync

--- models/staging/customers.sql
+++ models/staging/customers.sql
@@ -7,13 +7,11 @@
     Try changing "table" to "view" below
 */
 
-{{ config(materialized='table') }}
+{{ config(materialized='view') }}
 
 with source_data as (
 
-    select 1 as id
-    union all
-    select null as id
+   select * from {{ source('raw', 'customers') }}
 
 )
 
